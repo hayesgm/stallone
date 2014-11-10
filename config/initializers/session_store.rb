a056ccbbd481e518e74cfcc2bccfd1bbf00f879d
Geoff Hayes <hayesgm@gmail.com>
2014-11-09 16:39:57 -0800

    Use redis session

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_stallone_session'
+Rails.application.config.session_store :redis_store, key: '_stallone_session', redis_server: ENV["REDISCLOUD_URL"] || "redis://127.0.0.1:6379/0/session"
